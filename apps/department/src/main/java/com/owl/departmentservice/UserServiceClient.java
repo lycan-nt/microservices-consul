e562424048768ebc9d9f26ec7b66b9c8d6d81b02
Felipe D. Santos <lycan.felipe@gmail.com>
2023-12-20 12:15:49 -0300

    Remove org

--- apps/department/src/main/java/com/owl/departmentservice/UserServiceClient.java
+++ apps/department/src/main/java/com/owl/departmentservice/UserServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "usuario-service")
 public interface UserServiceClient {
 
     @GetMapping("/user/getUsersByDeptId")
